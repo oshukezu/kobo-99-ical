11518408910c25797f003e3149af99e192abcd24
J.J. Wang <oshukezu@gmail.com>
2025-12-16 18:27:52 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""Kobo 99 iCal Generator"""
-from .service import Kobo99ICalService
-from .config import Settings
-from .models import BookItem
-
-__all__ = ['Kobo99ICalService', 'Settings', 'BookItem']
-
